09ff231ac45af6f03b10a8ca061b8d5e8f9430c7
owostline <ostlinecasao@gmail.com>
2024-07-16 17:16:36 +0800

    working game

--- script.js
+++ script.js
@@ -2,85 +2,85 @@ let humanScore = 0, computerScore = 0;
 let humanChoice = '', computerChoice = '';
 let roundWinner = '';
 
-    function getComputerChoice() { // takes a random number from 1-3 and converts it 
-        while (computerChoice === '') {
-            computerChoice = Math.floor(Math.random() * 3)+1;
+const rock = document.getElementById("rock");
+rock.onclick = () => handleHumanChoice('rock');
+const paper = document.getElementById("paper");
+paper.onclick = () => handleHumanChoice('paper');
+const scissors = document.getElementById("scissors");
+scissors.onclick = () => handleHumanChoice('scissors');
 
-            if (computerChoice === 1) {
-                computerChoice = 'rock';
-            } else if (computerChoice === 2) {
-                computerChoice = 'paper';
-            } else if (computerChoice === 3) {
-                computerChoice = 'scissors';
-            } 
-        }
-    }
-    
-    function getHumanChoice() { //takes an answer from a prompt and converts it to lowercasd
-        while (humanChoice === '') {
-            humanChoice = window.prompt('Choose between ROCK, PAPER, or SCISSORS.' + '\n     YOU: ' + humanScore + '\n     COMPUTER: ' + computerScore);
-            
-            if (humanChoice.toLowerCase() === 'rock') {
-                humanChoice = 'rock';
-            } else if (humanChoice.toLowerCase() === 'paper') {
-                humanChoice = 'paper';
-            } else if (humanChoice.toLowerCase() === 'scissors') {
-                humanChoice = 'scissors';
-            } else {
-                humanChoice = '';
-                window.alert("Invalid input. Please try again.");
-            }
-        }
-    }
+function handleHumanChoice(choice) {
+    humanChoice = choice;
+    playGame();
+}
 
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    computerChoice = choices[Math.floor(Math.random() * 3)];
+}
 
+function updateChoiceImages() {
+    const humanChoiceImg = document.getElementById("pyrPokemon");
+    const computerChoiceImg = document.getElementById("oppPokemon");
 
-    function playRound() { //decides the winner, adds score, and displays who won
-        if (humanChoice === 'rock' && computerChoice === 'scissors') {
-            ++humanScore;
-            roundWinner = 'YOU: Rock vs COMP: Scissors\nYou win this round!'
-            alert(roundWinner);
-        } else if (humanChoice === 'rock' && computerChoice === 'paper'){
-            ++computerScore;
-            roundWinner = 'YOU: Rock vs COMP: Paper\nYou lost this round!'
-            alert(roundWinner);
-        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
-            ++computerScore;
-            roundWinner = 'YOU: Scissors vs COMP: Rock\nYou lost this round!'
-            alert(roundWinner);
-        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
-            ++humanScore;
-            roundWinner = 'YOU: Scissors vs COMP: Paper\nYou win this round!'
-            alert(roundWinner);
-        } else if (humanChoice === 'paper' && computerChoice === 'rock'){
-            ++humanScore;
-            roundWinner = 'YOU: Paper vs COMP: Rock\nYou win this round!'
-            alert(roundWinner);
-        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
-            ++computerScore;
-            roundWinner = 'YOU: Paper vs COMP: Scissors\nYou lost this round!'
-            alert(roundWinner);
-        } else {
-            alert('Both chose: ' + humanChoice.toUpperCase() + ". It's a TIE!");
-        }
-    }
+    humanChoiceImg.src = `img/pyr${humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1)}.png`;
+    computerChoiceImg.src = `img/opp${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}.png`;
+}
 
-    function playGame () { //game process, resets the both choices and round winners
-            getHumanChoice();
-            getComputerChoice();
-            playRound();
-            humanChoice = '';
-            computerChoice = '';
-            roundWinner = '';
-    }
+function playRound() {
+    const text = document.querySelector("h2");
+    const score = document.querySelector("h3");
 
-    while (humanScore < 5 && computerScore < 5) { //makes sure the game ends at max score of 5
-        playGame();
-    }
-
-    if (computerScore > humanScore) {
-        alert('You lost! The score is: ' + humanScore + ' - ' + computerScore);
+    if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        ++humanScore;
+        roundWinner = 'YOU: Rock vs COMP: Scissors. You win this round!';
+    } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        ++computerScore;
+        roundWinner = 'YOU: Rock vs COMP: Paper. You lost this round!';
+    } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        ++computerScore;
+        roundWinner = 'YOU: Scissors vs COMP: Rock. You lost this round!';
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        ++humanScore;
+        roundWinner = 'YOU: Scissors vs COMP: Paper. You win this round!';
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        ++humanScore;
+        roundWinner = 'YOU: Paper vs COMP: Rock. You win this round!';
+    } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        ++computerScore;
+        roundWinner = 'YOU: Paper vs COMP: Scissors. You lost this round!';
     } else {
-        alert('You win! The score is: ' + humanScore + ' - ' + computerScore);
+        roundWinner = 'Both chose: ' + humanChoice.toUpperCase() + ". It's a TIE!";
     }
 
+    text.textContent = roundWinner;
+    score.textContent = `Score - You: ${humanScore} | Computer: ${computerScore}`;
+    updateChoiceImages();
+}
+
+function playGame() {
+    getComputerChoice();
+    playRound();
+    humanChoice = '';
+    computerChoice = '';
+    roundWinner = '';
+    checkGameOver();
+}
+
+function checkGameOver() {
+    const text = document.querySelector("h2");
+    if (humanScore >= 5 || computerScore >= 5) {
+        if (computerScore > humanScore) {
+            text.textContent = 'You lost! The score is: ' + humanScore + ' - ' + computerScore;
+        } else {
+            text.textContent = 'You win! The score is: ' + humanScore + ' - ' + computerScore;
+        }
+        resetGame();
+    }
+}
+
+function resetGame() {
+    humanScore = 0;
+    computerScore = 0;
+    document.getElementById("score").textContent = `Score - You: ${humanScore} | Computer: ${computerScore}`;
+}
